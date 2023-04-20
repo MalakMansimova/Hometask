48612739352dd7a8619911c5e1654ca587ab0c42
MalakMansimova <7m094w3@code.edu.az>
2023-04-20 10:26:36 +0400

    commit

---
+++ Exception.cs/MyException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home20.Exception.cs
+{
+    internal class MyException:Exception
+    {
+        public MyException(string meesaage) :base(meesaage)
+        { 
+        
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Home20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name=Console.ReadLine();
+            string[] Array = new string[4];
+            try
+            {
+                AddToArray(Array, name);
+            }
+            catch (Exception Exc)
+            {
+
+                Console.WriteLine( Exc.Meesage);
+            }
+        }
+
+        static void AddToArray(string[] array, string name)
+        {
+            for (int i = 0; i < name.Length; i++) 
+            {
+                if (char.IsDigit(name[i]))
+                {
+                    throw new MyException("wrongg namee");
+                }
+            
+            }
+            Console.WriteLine("truee namee");
+        }
+    }
+}
